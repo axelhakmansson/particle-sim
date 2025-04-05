17a71472943edb17ed2476f4ada69b4921c3975f
hxppythxughts69 <axel-ekman-hakansson@hotmail.com>
2025-04-05 16:24:25 +0200

    some more setup for particles and forces

--- src/main.rs
+++ src/main.rs
@@ -3,39 +3,47 @@ use ggez::graphics::{self, Color};
 use ggez::{Context, GameResult};
 use ggez::glam::*;
 
+
+
 struct Particle {
-    pos_x: f32,
-    pos_y: f32
+    pos: Vec2,
+    acc: Vec2,
+    vel: Vec2
 }
 
 impl Particle {
-    fn new() -> GameResult<Particle> {
-        let s = Particle { pos_x: 0.0, pos_y: 0.0 };
+    fn new(pos: Vec2, acc: Vec2, vel: Vec2) -> GameResult<Particle> {
+        let s = Particle { pos, acc, vel };
         Ok(s)
+    }
+    fn gravity(&mut self) {
+        self.acc = Vec2 { x: 0.0, y: 0.982 };
     }
 }
 
 impl event::EventHandler<ggez::GameError> for Particle {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        self.pos_x = self.pos_x % 800.0 + 1.0;
+        self.vel += self.acc;
+        self.pos += self.vel;
+        self.acc *= 0.0;
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
         let mut canvas = graphics::Canvas::from_frame(
             ctx,
-            graphics::Color::from([0.1, 0.2, 0.3, 1.0]),
+            graphics::Color::WHITE,
         );
 
         let circle = graphics::Mesh::new_circle(
             ctx,
             graphics::DrawMode::fill(),
-            Vec2::new(0.0, 0.0),
-            100.0,
+            Vec2::ZERO,
+            10.0,
             2.0,
-            Color::WHITE,
+            Color::BLACK,
         )?;
-        canvas.draw(&circle, Vec2::new(self.pos_x, 380.0));
+        canvas.draw(&circle, self.pos);
 
         canvas.finish(ctx)?;
         Ok(())
@@ -45,6 +53,7 @@ impl event::EventHandler<ggez::GameError> for Particle {
 pub fn main() -> GameResult {
     let cb = ggez::ContextBuilder::new("super_simple", "ggez");
     let (ctx, event_loop) = cb.build()?;
-    let state = Particle::new()?;
-    event::run(ctx, event_loop, state)
+    let mut particle = Particle::new(Vec2 { x: 200.0, y: 30.0 }, Vec2::ZERO, Vec2::ZERO)?;
+    particle.gravity();
+    event::run(ctx, event_loop, particle)
 }
