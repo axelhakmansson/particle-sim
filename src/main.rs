a7791b44ef443779d7b096940378b50817d765b3
hxppythxughts69 <axel-ekman-hakansson@hotmail.com>
2025-04-17 17:34:59 +0200

    massa, radie, kollisioner

--- src/main.rs
+++ src/main.rs
@@ -6,21 +6,71 @@ use rand::prelude::*;
 
 const HEIGHT: f32 = 600.0;
 const WIDTH: f32 = 800.0;
-const PARTICLE_RADIUS: f32 = 10.0;
+const VEL_RANGE: f32 = 0.5;
+const ACC_RANGE: f32 = 0.5;
+const PARTICLE_MASS_HIGHER: f32 = 4.0;
+const PARTICLE_MASS_LOWER: f32 = 2.0;
 
+#[derive(PartialEq)]
 struct Particle {
     pos: Vec2,
     acc: Vec2,
-    vel: Vec2
+    vel: Vec2,
+    radius: f32,
+    mass: f32,
 }
 
 impl Particle {
-    fn new(pos: Vec2, acc: Vec2, vel: Vec2) -> GameResult<Particle> {
-        let particle = Particle { pos, acc, vel };
-        Ok(particle)
+    fn new() -> GameResult<Particle> {
+        let mut rng = rand::rng();
+        let mass = rng.random_range(PARTICLE_MASS_LOWER..PARTICLE_MASS_HIGHER);
+        Ok(Particle { 
+            pos: Vec2 { x: rng.random_range(0.0..WIDTH), y: rng.random_range(0.0..HEIGHT) }, 
+            acc: Vec2 { x: rng.random_range(-ACC_RANGE..ACC_RANGE), y: rng.random_range(-ACC_RANGE..ACC_RANGE) }, 
+            vel: Vec2 { x: rng.random_range(-VEL_RANGE..VEL_RANGE), y: rng.random_range(-VEL_RANGE..VEL_RANGE) },
+            radius: 3.0 * mass,
+            mass: mass,
+        })
     }
-    fn gravity(&mut self) {
-        self.acc = Vec2 { x: 0.0, y: 0.982 };
+
+    fn check_edge_collision(&mut self) {
+        if self.pos.x + self.radius > WIDTH {
+            self.pos.x = WIDTH - self.radius;
+            self.vel.x *= -1.0;
+        } else if self.pos.x - self.radius < 0.0 {
+            self.pos.x = self.radius;
+            self.vel.x *= -1.0;
+        }
+
+        if self.pos.y + self.radius > HEIGHT {
+            self.pos.y = HEIGHT - self.radius;
+            self.vel.y *= -1.0;
+        } else if self.pos.y - self.radius < 0.0 {
+            self.pos.y = self.radius;
+            self.vel.y *= -1.0;
+        }
+    }
+    fn check_particle_collision(&mut self, particles: Vec<&mut Particle>) {
+        for particle in particles {
+            if self != particle {
+                let distance = self.pos.distance(particle.pos);
+                let tolerance = self.radius + particle.radius;
+
+                if distance <= tolerance {
+                    let normal = (particle.pos - self.pos).normalize();
+                    let rel_vel = particle.vel - self.vel;
+                    let impulse = normal * (2.0 * rel_vel.dot(normal)) / (self.mass + particle.mass);
+
+                    let repulsion = normal * (tolerance - distance);
+
+                    self.vel += impulse / self.mass;
+                    particle.vel -= impulse / particle.mass;
+
+                    self.pos -= repulsion / self.mass;
+                    particle.pos += repulsion / particle.mass;
+                }
+            }
+        }
     }
 }
 
@@ -30,13 +80,9 @@ struct MainState {
 
 impl MainState {
     fn new(num_of_particles: u32) -> GameResult<MainState> {
-        let mut rng = rand::rng();
         let mut particles = vec![];
         for _ in 0..num_of_particles {
-            particles.push(Particle { 
-                pos: Vec2 { x: rng.random_range(0.0..WIDTH), y: rng.random_range(0.0..HEIGHT) }, 
-                acc: Vec2 { x: rng.random_range(-1.0..1.0), y: rng.random_range(-1.0..1.0) }, 
-                vel: Vec2 { x: rng.random_range(-1.0..1.0), y: rng.random_range(-1.0..1.0) } });
+            particles.push(Particle::new()?);
         }
         let state = MainState { particles };
         Ok(state)
@@ -46,10 +92,16 @@ impl MainState {
 
 impl event::EventHandler<ggez::GameError> for MainState {
     fn update(&mut self, _ctx: &mut Context) -> GameResult {
-        for particle in &mut self.particles {
+        for i in 0..self.particles.len() {
+            let (left, right) = self.particles.split_at_mut(i);
+            let (particle, rest) = right.split_first_mut().unwrap();
+            let combined_particles: Vec<_> = left.iter_mut().chain(rest.iter_mut()).collect();
+            particle.check_particle_collision(combined_particles);
+            particle.check_edge_collision();
             particle.vel += particle.acc;
             particle.pos += particle.vel;
             particle.acc *= 0.0;
+            particle.vel *= 1.0;
         }
         Ok(())
     }
@@ -65,7 +117,7 @@ impl event::EventHandler<ggez::GameError> for MainState {
                 ctx,
                 graphics::DrawMode::fill(),
                 Vec2::ZERO,
-                PARTICLE_RADIUS,
+                particle.radius,
                 2.0,
                 Color::BLACK,
             )?;
@@ -79,6 +131,6 @@ impl event::EventHandler<ggez::GameError> for MainState {
 pub fn main() -> GameResult {
     let cb = ggez::ContextBuilder::new("super_simple", "ggez");
     let (ctx, event_loop) = cb.build()?;
-    let mut state = MainState::new(10)?;
+    let state = MainState::new(50)?;
     event::run(ctx, event_loop, state)
 }
